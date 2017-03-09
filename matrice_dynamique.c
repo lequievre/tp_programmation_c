8d9d63c7699c0d537c170dd4394a45a360cc1d4e
Laurent LEQUIEVRE <laurent.lequievre@univ-bpclermont.fr>
2017-03-09 14:56:07 +0100

    Adding sample dynamic matrix

---
+++ matrice_dynamique.c
@@ -0,0 +1,100 @@
+/*
+ * Laurent LEQUIEVRE - Institut Pascal UMR6602 - 2017
+ * laurent.lequievre@univ-bpclermont.fr
+ * 
+ * Exemple de creation et utilisation d'une matrice d'entiers crees dynamiquement.
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MIN 5
+#define MAX 40
+
+/*
+ * Creation dynamique d'une matrice de 'nblignes' lignes et 'nbcolonnes' colonnes.
+ * 
+ */
+int ** creerMat(int nblignes, int nbcolonnes)
+{
+  int ** mat = malloc(nblignes * sizeof(int *));
+  int i;
+  for (i=0;i<nblignes;i++)
+  {
+	mat[i] = malloc(nbcolonnes * sizeof(int));
+  }
+
+  return mat;
+}
+
+/*
+ * Liberation d'une matrice dynamique de 'nblignes' lignes.
+ */
+void libererMat(int **mat, int nblignes)
+{
+  int i;
+  for (i=0; i<nblignes; i++)
+  {
+    free(mat[i]);
+  }
+  free(mat);
+}
+
+/*
+ * Initialisation d'une matrice dynamique de 'nblignes' lignes et 'nbcolonnes' colonnes
+ * avec des nombres aleatoires.
+ */
+void initMat(int **mat, int nblignes, int nbcolonnes)
+{
+   int i,j;
+   for (i=0; i<nblignes; i++)
+   {
+     for (j=0; j<nbcolonnes; j++)
+       mat[i][j] = rand()%(MAX-MIN+1)+MIN;
+   }
+}
+
+
+/*
+ * Affichage d'une matrice dynamique de 'nblignes' lignes et 'nbcolonnes' colonnes
+ */
+void afficheMat(int **mat, int nblignes, int nbcolonnes)
+{
+   int i,j;
+   for (i=0; i<nblignes; i++)
+   {
+     for (j=0; j<nbcolonnes; j++)
+       printf(" %d",mat[i][j]);
+     printf("\n");
+   }
+}
+
+
+
+int main()
+{
+  // Declaration de la matrice qui est un pointeur de pointeurs.
+  int **mat;
+
+  // Definition du nb de lignes et de colonnes de la matrice.
+  int nblignes = 5;
+  int nbcolonnes = 3;
+  
+  // Initialisation du tirage aleatoire.
+  srand(time(NULL));
+
+  // Allocation dynamique de la matrice.
+  mat = creerMat(nblignes,nbcolonnes);
+  
+  // Remplissage de la matrice avec des nombres aleatoires.
+  initMat(mat,nblignes,nbcolonnes);
+  
+  // Affichage de la matrice.
+  afficheMat(mat,nblignes,nbcolonnes);
+
+  // Liberation de la matrice.
+  libererMat(mat,nblignes);
+
+}
